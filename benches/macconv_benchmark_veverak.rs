31230f6861b3f70071fbd14b666773e0e78a40c8
Roman <zajic@zajic.net>
2023-02-06 18:48:17 +0800

    Comments from Veverak included

--- benches/macconv_benchmark_veverak.rs
+++ benches/macconv_benchmark_veverak.rs
@@ -1,5 +1,21 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
+// Comments by Veverak
+// Vec and String are complex types that allocate memory and are optimized for pushing an unbounded
+// amount of items to the end. You don't need them. I would use an array for the buffer.
+// That is [u8; 17]. Unfortunately there is no standard type for owned strings backed by a primitive array,
+// so you have to make your own. I would make a type that wraps the array and implements the trait Deref<str>.
+//
+// The loop contains a match expression with quite a lot of conditions. I would unroll the loop to write
+// two digits at a time and then write the delimiter.
+//
+// Since it's clear that there will never be any invalid UTF-8, I would use the unchecked variant of
+// String::from_utf8 (or better str::from_utf8 in the Deref implementation suggested above).
+//
+// As we're starting to get comfortable with unsafe code after applying the last suggestion,
+// we can as well make the buffer [std::mem::MaybeUninit::uninit(); 17] and only write to each byte once.
+
+
 pub struct FormattedMac {
     inner: [u8; 17],
 }
